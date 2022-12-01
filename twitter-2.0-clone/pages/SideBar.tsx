e10a77eaac2b1d3947549820425d898d1c1a8a16
fortune <omorodion.fortune@gmail.com>
2022-12-01 10:25:23 +0100

    building the widgets

--- twitter-2.0-clone/pages/SideBar.tsx
+++ twitter-2.0-clone/pages/SideBar.tsx
@@ -13,14 +13,16 @@ import SidebarRow from "./SidebarRow";
 
 function SideBar() {
   return (
-    <div>
+    <div className="flex flex-col">
       <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
       <SidebarRow Icon={HomeIcon} title="Home" />
       <SidebarRow Icon={HashtagIcon} title="Explore" />
       <SidebarRow Icon={BellIcon} title="Notification" />
       <SidebarRow Icon={MailIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmark" />
-        <SidebarRow Icon={CollectionIcon} title="List" />
+      <SidebarRow Icon={CollectionIcon} title="List" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
   );
 }
--- twitter-2.0-clone/pages/index.tsx
+++ twitter-2.0-clone/pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "./Feed";
 import SideBar from "./SideBar";
+import Widgets from "./Widgets";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
       <main>
         <SideBar/>
+        <Feed/>
+        <Widgets/>
       </main>
     </div>
   );
